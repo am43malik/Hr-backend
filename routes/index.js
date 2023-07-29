6a3eb2e4f3bc11c42351ffbf25f5972aa9070d49
Faisal <iamfaisalkhaan@gmail.com>
2023-07-29 18:32:17 +0530

    forms updates

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ import LoginSchema from '../controllers/Auth/loginController'
 import userController from '../controllers/Auth/userController'
 import auth from '../middlewares/auth'
 import newEmployeeController from '../controllers/Forms/newEmployeeController'
+import endofServicesController from '../controllers/Forms/endofServicesController'
+import exitForLeaveController from '../controllers/Forms/exitForLeaveController'
 
 
 
@@ -24,6 +26,21 @@ Route.put('/updateEmployee/:id', auth,newEmployeeController.updateEmployee)
 Route.delete('/deleteEmployee/:id', auth,newEmployeeController.deleteEmployee)
 //--------------------------------allEmployee--------------------------------------
 
+//----------------------end of services-----------------------------
+Route.get('/allEndofservice/',endofServicesController.allEndofservice)
+Route.get('/oneEndofservice/:id',endofServicesController.oneEndofservice)
+Route.post('/endofservices', auth,endofServicesController.endofservices)
+Route.put('/UpdateEndofservices/:id', auth,endofServicesController.UpdateEndofservices)
+Route.delete('/deleteEndofservice/:id', auth,endofServicesController.deleteEndofservice)
+//-----------------------------enf of services----------------------------------------
+
+//=========================exit for leave======================================
+Route.get('/allExitofleave/',exitForLeaveController.allExitofleave)
+Route.get('/oneExitofleave/:id',exitForLeaveController.oneExitofleave)
+Route.post('/exitofleave', auth,exitForLeaveController.exitofleave)
+Route.put('/updateExitofleave/:id', auth,exitForLeaveController.updateExitofleave)
+Route.delete('/deleteExitofleave/:id', auth,exitForLeaveController.deleteExitofleave)
+//=====================================================================================
 
 
 
